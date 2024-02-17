cb361d27f7be0eaa2dbd2993777e7dc4acd50ace
ogadarita <rita.writescontent@gmail.com>
2024-02-17 04:33:27 -0800

    my_table.c

---
+++ my_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* Return: void
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("%d ", j);
+		}
+		printf("%d\n ", i);
+	}
+	return (0);
+}

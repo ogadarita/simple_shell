fd74daa25279fba892e42f0ba629b934952f218e
ogadarita <rita.writescontent@gmail.com>
2024-02-19 22:21:04 -0800

    shell header files

--- my_table.c
+++ my_table.c
@@ -9,6 +9,8 @@ int main(void)
 {
 	int i;
 	int j;
+	getpid();
+
 
 	for (i = 0; i < 10; i++)
 	{
---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void disPrompt(void);
+void execCmd(const char * my_cmd);
+void myPrintFunc(const char * promptMsg);
+void readCmd(char * my_cmd, size_t size);
+
+#endif
